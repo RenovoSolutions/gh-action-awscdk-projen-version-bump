a91fa1c41ed31c8ee282a8076647881d5d76bff5
Brandon Miller <brandon@digital-reboot.com>
2022-01-10 17:26:46 -0500

    Init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # gh-action-awscdk-projen-version-bump
-GitHub action that upgrades target AWS CDK version within Projen
+## Get latest AWS CDK release and update projen docker actions
+
+This action gets the latest AWS CDK release version for a given major version of the AWS CDK and then updates the projects [projen](https://github.com/projen/projen) configuration to use this new version.
+
+## Inputs
+
+## `major-version`
+
+**Required** The major version to get releases for.
+
+## Example usage
+
+```
+uses: renovosolutions/gh-action-awscdk-projen-version-bump@v1
+with:
+  major-version: 2
+```
---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <cdk-major-version>"
+  exit 1
+fi
+
+if echo "$2" | grep -Eq '^[0-9]+$'; then
+  echo "Error: CDK major version must be a number"
+  exit 1
+fi
+
+release_version=$( curl -sL https://api.github.com/repos/aws/aws-cdk/releases | jq -r .[].tag_name | grep "v$1" | sort -V -r | head -1 | sed 's/v//' )
+local_version=$( grep cdkVersion .projenrc.js | awk ' { print $2 } ' | sed -e "s/'//g" -e 's/,//' )
+
+if [  "$release_version" = "$local_version" ]; then
+  echo "No need to upgrade CDK version"
+else
+  echo "Upgrading CDK version from $local_version to $release_version"
+  sed -i "s/cdkVersion: '$local_version'/cdkVersion: '$release_version'/g" .projenrc.js
+  # remove the lock file so we can update appropriately for the new CDK version
+  rm yarn.lock
+  if npx projen && jest test; then
+    echo "CDK version upgraded successfully"
+  else
+    echo "CDK version upgrade failed"
+    exit 1
+  fi
+fi
